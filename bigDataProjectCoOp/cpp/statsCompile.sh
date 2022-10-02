8e4f2476af0a19dc00c74f78a8041c3a69ee1f9c
Aryo Yarahmadi <aryoyarahmadi@Aryos-MacBook-Pro.local>
2022-10-02 17:31:57 -0500

    compare mode added2

--- bigDataProjectCoOp/cpp/statsCompile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++17 matrixmem.h matrixmem.o datasettxt.h datasettxt.o csv.h csv.o stats_carlos.cpp foundation.h foundation.o param.h param.o str.h str.o textio.h textio.o trace.h trace.o -o ../bin/stats
-
